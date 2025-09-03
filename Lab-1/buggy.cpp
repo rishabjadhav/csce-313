5173f94e949350e0bc3e3218fafa84f659ccbc30
rishabjadhav <rishabjadhav@gmail.com>
2025-09-03 21:18:30 +0000

    Fixed bugs, finished lab

--- Lab-1/buggy.cpp
+++ Lab-1/buggy.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 struct Point {
     int x, y;
@@ -11,57 +12,77 @@ class Shape {
     int vertices;
     Point** points;
 
-    Shape (int _vertices) {
-        vertices = _vertices;
-        points = new Point*[vertices+1];
-    }
+    public:
+        Shape (int _vertices) {
+            vertices = _vertices;
+            points = new Point*[vertices+1];
+        }
 
     ~Shape () {
+        // remember to delete dynamically allocated variables in the destructor
+        delete[] points;
     }
 
-    void addPoints (/* formal parameter for unsized array called pts */) {
-        for (int i = 0; i <= vertices; i++) {
-            memcpy(points[i], &pts[i%vertices], sizeof(Point));
+    void addPoints (Point** pts) {
+        for (int i = 0; i < vertices; i++) {
+            // copy over memory from each element in pts into points
+            memcpy(&points[i], &pts[i%vertices], sizeof(Point));
         }
     }
 
-    double* area () {
+    // corrected return type of area() function
+    double area () {
         int temp = 0;
-        for (int i = 0; i <= vertices; i++) {
-            // FIXME: there are two methods to access members of pointers
-            //        use one to fix lhs and the other to fix rhs
-            int lhs = points[i].x * points[i+1].y;
-            int rhs = points[i+1].x * points[i].y;
+        for (int i = 0; i < vertices; i++) {
+            // use % vertices to wrap around to first element and avoid invalid access
+            int lhs = (*points[i]).x * (*points[(i+1)%vertices]).y;
+            int rhs = points[(i+1)%vertices]->x * points[i]->y;
             temp += (lhs - rhs);
         }
         double area = abs(temp)/2.0;
-        return &area;
+        return area;
     }
 };
 
 int main () {
-    // FIXME: create the following points using the three different methods
+    // create the following points using the three different methods
     //        of defining structs:
     //          tri1 = (0, 0)
     //          tri2 = (1, 2)
     //          tri3 = (2, 0)
 
-    // adding points to tri
-    Point triPts[3] = {tri1, tri2, tri3};
-    Shape* tri = new Shape(3);
-    tri.addPoints(triPts);
+    Point _tri1 = {0, 0};
+    Point _tri2; _tri2.x = 1; _tri2.y = 2;
+    Point _tri3{2, 0};
 
-    // FIXME: create the following points using your preferred struct
-    //        definition:
-    //          quad1 = (0, 0)
-    //          quad2 = (0, 2)
-    //          quad3 = (2, 2)
-    //          quad4 = (2, 0)
+    Point* tri1 = &_tri1;
+    Point* tri2 = &_tri2;
+    Point* tri3 = &_tri3;
+
+    // adding points to tri
+    Point* triPts[3] = {tri1, tri2, tri3};
+    Shape* tri = new Shape(3);
+    tri->addPoints(triPts);
 
     // adding points to quad
-    Point quadPts[4] = {quad1, quad2, quad3, quad4};
-    Shape* quad = new Shape(4);
-    quad.addPoints(quadPts);
+    Point* quad1 = new Point(0, 0);
+    Point* quad2 = new Point(0, 2);
+    Point* quad3 = new Point(2, 2);
+    Point* quad4 = new Point(2, 0);
 
-    // FIXME: print out area of tri and area of quad
+    Point* quadPts[4] = {quad1, quad2, quad3, quad4};
+    Shape* quad = new Shape(4);
+    quad->addPoints(quadPts);
+
+    std::cout << tri->area() << std::endl;
+    std::cout << quad->area() << std::endl;
+
+    // delete all dynamically allocated objects
+    delete tri;
+
+    delete quad1;
+    delete quad2;
+    delete quad3;
+    delete quad4;
+    delete quad;
 }
