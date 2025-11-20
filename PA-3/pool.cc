0b816a7c590219b6db548a920f41037e03df54d7
rishabjadhav <rishabjadhav@gmail.com>
2025-11-20 09:32:05 +0000

    completed PA3

--- PA-3/pool.cc
+++ PA-3/pool.cc
@@ -2,6 +2,9 @@
 #include <mutex>
 #include <iostream>
 
+// mutex to serialize console output so lines don't get interleaved
+static std::mutex print_mtx;
+
 Task::Task() = default;
 Task::~Task() = default;
 
@@ -23,21 +26,92 @@ ThreadPool::~ThreadPool() {
     queue.clear();
 }
 
+void ThreadPool::WaitForTask(const std::string &name) {
+    // unique_lock prevents multiple threads from accessing our task_status map at once
+    std::unique_lock<std::mutex> lock(mtx);
+
+    // find task with name name in task_status
+    auto it = task_status.find(name);
+    if (it == task_status.end()) {
+        throw std::runtime_error("unknown task name");
+    }
+
+    TaskStatus &status = it->second;
+
+    // wait until task is finished
+    while (!status.finished) {
+        status.cv.wait(lock);
+    }
+
+    task_status.erase(it);
+}
+
 void ThreadPool::SubmitTask(const std::string &name, Task *task) {
-    //TODO: Add task to queue, make sure to lock the queue
+    // if Stop() called, do not allow tasks to be submitted
+    if (done) {
+        {
+            std::lock_guard<std::mutex> pl(print_mtx);
+            std::cout << "Cannot added task to queue" << std::endl;
+        }
+        return;
+    }
+
+    mtx.lock(); // prevents multiple threads from accessing queue at once
+
+    queue.push_back(task); // add task to queue
+    
+    task->name = name;
+    task_status[name].task = task;
+    task_status[name].finished = false;
+
+    mtx.unlock(); // unlock mutex to allow for other thread control after queue access
+
+    {
+        std::lock_guard<std::mutex> pl(print_mtx);
+        std::cout << "Added task " << name << std::endl;
+    }
+
+    cv.notify_one();
 }
 
 void ThreadPool::run_thread() {
     while (true) {
+        std::unique_lock<std::mutex> lock(mtx);
 
-        //TODO1: if done and no tasks left, break
+        // wait until there's work or we're done
+        cv.wait(lock, [this] { return done || !queue.empty(); });
 
-        //TODO2: if no tasks left, continue
+        // done is true and queue is empty, all threads are done/released
+        if (done && queue.empty()) {
+            {
+                std::lock_guard<std::mutex> pl(print_mtx);
+                std::cout << "Stopping thread" << std::endl;
+            }
+            break;
+        }
 
-       
-        //TODO3: get task from queue, remove it from queue, and run it
+        // get task from queue and remove it
+        Task *t = queue.front();
+        queue.erase(queue.begin());
+        lock.unlock();
 
-        //TODO4: delete task
+        {
+            std::lock_guard<std::mutex> pl(print_mtx);
+            std::cout << "Started task " << t->name << std::endl;
+        }
+        t->Run();
+        {
+            std::lock_guard<std::mutex> pl(print_mtx);
+            std::cout << "Finished task " << t->name << std::endl;
+        }
+
+        // second lock to prevent multi-thread access to task_status
+        std::unique_lock<std::mutex> lock2(mtx);
+        task_status[t->name].finished = true;
+        task_status[t->name].cv.notify_one();
+        lock2.unlock();
+
+        delete t;
     }
 }
 
@@ -56,5 +130,26 @@ void ThreadPool::remove_task(Task *t) {
 }
 
 void ThreadPool::Stop() {
-    //TODO: Delete threads, but remember to wait for them to finish first
+    {
+        std::lock_guard<std::mutex> pl(print_mtx);
+        std::cout << "Called Stop()" << std::endl;
+    }
+
+    mtx.lock();
+
+    // enable done to signal Stop() called
+    done = true;
+
+    // awakens all waiting threads
+    cv.notify_all();
+    mtx.unlock();
+
+    // join all threads
+    for (std::thread* t : threads) {
+        if (t->joinable()) {
+            t->join();
+        }
+        delete t;
+    }
+    threads.clear();
 }
--- PA-3/pool.h
+++ PA-3/pool.h
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <semaphore>
+#include <unordered_map>
 
 class Task {
 public:
@@ -11,8 +12,8 @@ public:
 
     virtual ~Task();
 
-    virtual void Run() = 0;  // implemented by subclass
-    bool is_running() const { return running; }
+    virtual void Run() = 0;  // implemented by subclass, virtual function!!
+    bool is_running() const { return running; } // returns if Task is running
 
     std::string name;
     bool running = false;
@@ -20,24 +21,36 @@ public:
 
 class ThreadPool {
 public:
-    explicit ThreadPool(int num_threads);
+    explicit ThreadPool(int num_threads); // initialized with number of threads
 
     ~ThreadPool();
 
     // Submit a task with a particular name.
-    void SubmitTask(const std::string &name, Task *task);
+    void SubmitTask(const std::string &name, Task *task); // takes a task with name
+    void WaitForTask(const std::string &name);
     void remove_task(Task *t);
 
     // Stop all threads. All tasks must have been waited for before calling this.
     // You may assume that SubmitTask() is not caled after this is called.
     void Stop();
 
-    void run_thread();
+    void run_thread(); // 
 
     int num_tasks_unserviced = 0;
+
+    // struct to link status of task to task itself
+    struct TaskStatus {
+        bool finished = false;
+        std::condition_variable cv;
+        Task* task = nullptr;
+    };
+
+    // used by WaitForTask to map name to status of task
+    std::unordered_map<std::string, TaskStatus> task_status;
 private:
     std::mutex mtx;
-    std::vector<std::thread *> threads;
-    std::vector<Task *> queue;
-    volatile bool done = false;
+    std::condition_variable cv;
+    std::vector<std::thread *> threads; // holds all threads
+    std::vector<Task *> queue; // holds all tasks that haven't been executed yet
+    volatile bool done = false; // set to true once all tasks done (queue empty)
 };
